cf7151bea13ab1236d3a2f70b26bdc782df86e11
georgetonjr <37754623+georgetonjr@users.noreply.github.com>
2020-11-20 14:56:10 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const routes = require('./routes');
 const cors = require('cors');
-const DB = require('./DataBase')
+const DB = require('./DataBase');
 
 const app = express();
 
